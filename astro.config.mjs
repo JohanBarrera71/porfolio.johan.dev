117b492c9e14600e391360b52949e7e64dfc5785
JohanBarrera71 <johan.dev.71@gmail.com>
2025-03-19 11:14:21 -0600

    Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.johan.dev/'
-})
+  site: "https://porfolio.johan.dev/",
+  base: "/porfolio.johan.dev/",
+});
